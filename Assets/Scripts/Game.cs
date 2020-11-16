67f62e2d88e44607ebb84c209365e0e7999dcefa
aidansmcdonald <aidans.mcdonald@gmail.com>
2020-11-16 18:10:06 -0500

    Chapter 10 - Added robot spawning and spawnpoints

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+    [SerializeField]
+    RobotSpawn[] spawns;
+    public int enemiesLeft;
+    // Start is called before the first frame update
+   
+    // Initialize Singleton / Spawn Robots
+    void Start()
+    {
+        singleton = this;
+        SpawnRobots();
+    }
+    // Goes through each robot in the array and spawns them
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
