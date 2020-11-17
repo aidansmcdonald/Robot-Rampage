85da4e7493afb482d35f04ad6f05ae4e4d38e2f1
aidansmcdonald <aidans.mcdonald@gmail.com>
2020-11-17 16:21:18 -0500

    Chapter 11 - Added start menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Loads battle scene when begin button is pressed
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+    // Exits the app (Will not exit unity)
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        
+    }
+}
